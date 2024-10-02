bd64951e1f3bb754bdcd6a81de3a28fdfcd41b88
Aldo Jimenez Paniagua <thatmetalfestlml@hotmail.com>
2024-10-02 01:49:25 -0600

    fix: change settings to getenv variables

--- src/config.py
+++ src/config.py
@@ -1,16 +1,21 @@
+import os
 from pydantic_settings import BaseSettings
+from pydantic import ValidationError
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     # Se declaran las variables de entorno a utilizar
-    secret_key: str
-    algorithm : str
-    expire_time: int
-    database_url: str
-
-    class Config:
-        env_file = ".env"
-
+    secret_key: str = os.getenv("SECRET_KEY")
+    algorithm : str = os.getenv("ALGORITHM", "HS256")
+    expire_time: int = os.getenv("EXPIRE_TIME", 10)
+    database_url: str = os.getenv("DATABASE_URL")
 
 # Se crea una instancia de settings para consumo de variables de entorno
-settings = Settings()
+try:
+    settings = Settings()
+except ValidationError as e:
+    print(e, "*"*100)
